e4c4cee9955e08d090fe054c9254ce98e1617be5
German Rueda <germanrueda@gmail.com>
2018-09-08 20:56:23 +0000

    add user profiles

--- models/user.js
+++ models/user.js
@@ -4,7 +4,12 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
+    avatar: {type: String, default: "https://djqvcbmmgpti5.cloudfront.net/assets/accounts/default-avatar-c7865b464e0a56a7e88d8c8b1fe45ba928adddcd58022b94d0527c4cbd729d39.png"},
+    firstName: String,
+    lastName: String,
+    email: String,
+    about: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,15 @@ router.get("/register", function(req, res){
 
 // handle sign up logic
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+            username: req.body.username, 
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            avatar: req.body.avatar,
+            email: req.body.email,
+            about: req.body.about
+        });
+    // eval(require("locus"));
     if(req.body.adminCode === "secretcode123"){
         newUser.isAdmin = true;
     }
@@ -51,6 +59,45 @@ router.get("/logout", function(req, res){
     req.logout();
     req.flash("success", "Succesfully logged out!");
     res.redirect("/campgrounds");
+});
+
+// Index user profile
+router.get("/users/:id", function(req, res){
+   User.findById(req.params.id, function(err, foundUser){
+       if(err){
+           console.log(err);
+           req.flash("error", "Something went wrong with user profile!")
+           res.redirect("back")
+       } 
+       res.render("users/show", {user: foundUser});
+   });
+});
+
+// Show EDIT form for user
+router.get("/users/:id/edit", function(req, res){
+   User.findById(req.params.id, function(err, foundUser){
+       if(err){
+           console.log(err);
+           req.flash("error", "Cannot find profile!")
+           res.redirect("back")
+       } 
+       res.render("users/edit", {user: foundUser});
+   });
+});
+
+
+// UPDATE user profile 
+router.put("/users/:id/edit", function(req, res){
+    User.findByIdAndUpdate(req.params.id, req.body.updatedInfo, function(err, foundUser){
+        if(err){
+            console.log(err);
+            req.flash("error", "Could Not Update User");
+            res.redirect("back");
+        } else{
+            req.flash("success","Profile Successfully Updated!");
+            res.redirect("/users/" + foundUser._id);
+        }
+    });
 });
 
 module.exports = router;
